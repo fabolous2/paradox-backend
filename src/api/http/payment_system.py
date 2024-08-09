0bec2251e4a12e3716ab2e23b61c4feebb81593b
yuapX <shesterovmm@gmail.com>
2024-08-09 15:43:37 +0300

    add payment router + top up method

--- src/api/http/payment_system.py
+++ src/api/http/payment_system.py
@@ -4,10 +4,10 @@ from fastapi.responses import JSONResponse
 from dishka import FromDishka
 from dishka.integrations.fastapi import DishkaRoute
 
-from src.services import UserService
+from src.services import BileeService
 from src.api.schema.referral import ReferralCode, NewReferralCode
 from src.main.config import settings
-from src.api.schema.payment import RawSumma, TopUpMethod
+from src.api.schema.payment import TopUpSchema
 
 
 router = APIRouter(
@@ -19,15 +19,9 @@ router = APIRouter(
 
 @router.post('/')
 async def top_up(
-    user_id: int,
-    summa: RawSumma,
-    method: TopUpMethod,
-    user_service: FromDishka[UserService],
+    data: TopUpSchema,
+    bilee_service: FromDishka[BileeService],
 ) -> JSONResponse:
-    user = await user_service.get_one_user(user_id=user_id)
-    if not user:
-        return JSONResponse(
-            status_code=404,
-            content=dict(detail='User not found.')
-        )
-    
+    response = bilee_service.create_invoice(amount=data.amount, method=data.method.value)
+
+    return JSONResponse(status_code=200, content=dict(service_response=response))
--- src/api/schema/payment.py
+++ src/api/schema/payment.py
@@ -1,14 +1,13 @@
 from enum import Enum
-from decimal import Decimal
 
 from pydantic import BaseModel, Field
-
-
-class RawSumma(BaseModel):
-    amount: Decimal = Field(ge=10, le=50000)
 
 
 class TopUpMethod(Enum):
     SBP = 'sbp'
     CARD = 'card'
     
+
+class TopUpSchema(BaseModel):
+    amount: float = Field(ge=10, le=50000)
+    method: TopUpMethod = Field(default=TopUpMethod.CARD)
